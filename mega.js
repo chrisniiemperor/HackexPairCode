a2141627d1f3ef3d8fdbf7472f8485962d5eb061
CHRIS-TECH <129108885+chrisniiemperor@users.noreply.github.com>
2026-07-30 22:17:30 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'chrisniiemperor@gmail.com', // Replace with your Mega email
-    password: 'Chris11204', // Replace with your Mega password
+    email: 'example@gmail.com', // Replace with your Mega email
+    password: 'password123', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
